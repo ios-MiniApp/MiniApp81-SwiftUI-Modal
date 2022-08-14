315208877e9481cffa9aabcb8df537c270093b62
Kota <maedakoutaa@gmail.com>
2022-08-14 10:54:40 +0900

    Initial Commit

---
+++ MiniApp81-SwiftUI-Modal/MiniApp81_SwiftUI_ModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp81_SwiftUI_ModalApp.swift
+//  MiniApp81-SwiftUI-Modal
+//
+//  Created by 前田航汰 on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp81_SwiftUI_ModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
